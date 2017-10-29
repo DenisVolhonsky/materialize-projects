c55493a41fc787ce88bf38acaefc76fdd666cd44
DenisVolhonsky <dvolhonsky@gmail.com>
2017-10-29 21:11:05 +0200

    plitka image masonri

---
+++ plitka masonri/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+
+
+    $('.grid').imagesLoaded(function() {
+        $('.grid').masonry({
+            itemSelector: '.grid-item',
+            columnWidth: '.grid-sizer',
+            percentPosition: true
+        });
+    });
+});
+
+
+
